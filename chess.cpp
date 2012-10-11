8376e37c3d1e6e9d21041b150b77a3357272f9db
SJ Ong <shingjay.ong@gmail.com>
2012-10-11 14:09:57 -0400

    Completed Part 1

---
+++ chess.cpp
@@ -0,0 +1,195 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <sched.h>
+#include <dlfcn.h>
+#include <map>
+#include <stdio.h>
+
+using namespace std;		
+
+/* thread statuses*/
+#define RUNNING_WITH_LOCK	1; //thread runs and does not need lock
+#define RUNNING_NO_LOCK		2; //threads runs and is waiting for lock
+#define TERMINATE 			3; //thread terminated
+
+int (*original_pthread_create)(pthread_t*, const pthread_attr_t*, void* (*)(void*), void*) = NULL;
+int (*original_pthread_join)(pthread_t, void**) = NULL;
+int (*original_pthread_mutex_unlock)(pthread_mutex_t*) = NULL;
+int (*original_pthread_mutex_lock)(pthread_mutex_t*) = NULL;
+
+static pthread_mutex_t	globalLock = PTHREAD_MUTEX_INITIALIZER; //global lock
+static pthread_t		current = 0;
+
+
+static map<pthread_mutex_t*, pthread_t> mutexMap; //maps mutex lock to its value
+static map<pthread_t, int> threadMap; //maps thread to its status
+
+static void initialize_original_functions();
+
+struct Thread_Arg {
+    void* (*start_routine)(void*);
+    void* arg;
+};
+
+static
+void* thread_main(void *arg)
+{
+    struct Thread_Arg thread_arg = *(struct Thread_Arg*)arg;
+    free(arg);
+    
+	// so now set the status of the new thread	
+    threadMap[pthread_self()] = RUNNING_NO_LOCK;
+    
+	// spinlock
+	while(pthread_equal(pthread_self(),current) == 0) {}
+	
+	// lock the global lock
+	original_pthread_mutex_lock(&globalLock);
+	
+    void* ret = thread_arg.start_routine(thread_arg.arg); //call second thread	
+	// thread stops running
+	threadMap[pthread_self()] = TERMINATE;
+	
+    original_pthread_mutex_unlock(&globalLock);
+
+    return ret;
+}
+
+extern "C"
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                   void *(*start_routine)(void*), void *arg)
+{
+    initialize_original_functions();
+	
+    struct Thread_Arg *thread_arg = (struct Thread_Arg*)malloc(sizeof(struct Thread_Arg));
+    thread_arg->start_routine = start_routine;
+    thread_arg->arg = arg;
+	
+	// global lock is locked now (first instance of create a new thread)
+	original_pthread_mutex_lock(&globalLock);
+	
+    int ret = original_pthread_create(thread, attr, thread_main, thread_arg);
+    
+    return ret;
+}
+
+extern "C"
+int pthread_join(pthread_t joinee, void **retval)
+{
+    initialize_original_functions();
+	original_pthread_mutex_unlock(&globalLock);
+	
+	/* select next available thread to execute */
+	/* wait till joinee terminates */
+	int x = TERMINATE;
+	
+	if (threadMap[joinee] != x)
+		current = joinee;
+	
+
+	while(threadMap[joinee] != x) {}
+	
+	original_pthread_mutex_lock(&globalLock);	
+	
+    return original_pthread_join(joinee, retval);
+}
+
+extern "C"
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+    initialize_original_functions();
+	
+	// check if *mutex is held by other threads.
+	// but it needs to be in the map.
+	
+	// if mutex is held by other threads 
+	// if thread in map is different from the current thread
+	if (mutexMap[mutex] != 0)
+	{
+		// unlock the global lock first
+		original_pthread_mutex_unlock(&globalLock);
+			
+		// select next available thread to execute; ie: find the next thread available to run
+		
+		current = mutexMap[mutex];
+		threadMap[current] = RUNNING_WITH_LOCK;
+		
+		// wait till L is not held by other threads; ie wait till its done running
+		while(pthread_equal(pthread_self(),current) == 0) {}
+		
+		// now the running thread is the other one
+		current = pthread_self();
+		
+		original_pthread_mutex_lock(&globalLock);	
+		
+		// lock the mutex and change status of thread
+		mutexMap[mutex] = current;
+		threadMap[current] = RUNNING_WITH_LOCK;
+	}
+	else
+	{
+		current = pthread_self();
+		mutexMap[mutex] = current;
+		threadMap[current] = RUNNING_WITH_LOCK;	
+	}
+
+    return original_pthread_mutex_lock(mutex);
+}
+
+extern "C"
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+    initialize_original_functions();
+	
+	// if L belongs to current thread, then UNLOCK(L)
+	if (pthread_equal(mutexMap[mutex],pthread_self()) != 0)
+		mutexMap[mutex] = 0;
+    
+    return original_pthread_mutex_unlock(mutex);
+}
+
+extern "C"
+int sched_yield(void)
+{
+	original_pthread_mutex_unlock(&globalLock);
+
+	// select next available thread to execute
+	// if there's no other available thread, the calling thread will execute
+	// if there's another available thread, the other thread should be executed
+	
+	map<pthread_t,int>::iterator thread_it;
+	
+	// iterate through each thread in the map
+	for (thread_it = threadMap.begin() ; thread_it != threadMap.end(); thread_it++)
+	{
+		int x = RUNNING_NO_LOCK;
+		// check if the thread is running with no lock
+		if (thread_it->second == x) {
+			// then let it run instead of the current thread
+			current = thread_it->first;
+			break;
+		}
+	}
+	
+	while(pthread_equal(pthread_self(),current) == 0) {}
+	original_pthread_mutex_lock(&globalLock);
+    return 0;
+}
+
+static
+void initialize_original_functions()
+{
+    static bool initialized = false;
+    if (!initialized) {
+        initialized = true;
+
+        original_pthread_create =
+            (int (*)(pthread_t*, const pthread_attr_t*, void* (*)(void*), void*))dlsym(RTLD_NEXT, "pthread_create");
+        original_pthread_join =
+            (int (*)(pthread_t, void**))dlsym(RTLD_NEXT, "pthread_join");
+        original_pthread_mutex_lock =
+            (int (*)(pthread_mutex_t*))dlsym(RTLD_NEXT, "pthread_mutex_lock");
+        original_pthread_mutex_unlock =
+            (int (*)(pthread_mutex_t*))dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+    }
+}
